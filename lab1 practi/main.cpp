ee9cfbccd2ce2fdad68eaa14ebed873b187f5379
kqlae <kqlae@DESKTOP-C2SHSNG>
2025-09-26 19:18:09 +0700

    input,out put, radian

--- lab1 practi/main.cpp
+++ lab1 practi/main.cpp
@@ -1,33 +1,61 @@
+#include <iostream> 
+#include <iomanip>
+#include <cassert>
+#include <sstream>
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 class Angle {
 private:
-    int degrees; 
-    int minutes; // 0-59
+    int degrees;
+    int minutes;
 
-    
+
     void normalize() {
-        // all to minut
         int totalMinutes = degrees * 60 + minutes;
-
-        // podrobno [-360*60, 360*60)
         int minutesInFullCircle = 360 * 60;
         totalMinutes = totalMinutes % minutesInFullCircle;
-
-        // - come to+
         if (totalMinutes < 0) {
             totalMinutes += minutesInFullCircle;
         }
-
-        // all gradus
         degrees = totalMinutes / 60;
         minutes = totalMinutes % 60;
-
-        // 360
         if (degrees >= 360) {
             degrees = 0;
             minutes = 0;
         }
+    }
+public:
+    // --- 1)
+    Angle() : degrees(0), minutes(0) {}
+
+    Angle(int deg, int min) : degrees(deg), minutes(min) {
+        normalize();
+        assert(degrees >= 0 && degrees < 360);
+        assert(minutes >= 0 && minutes < 60);
+    }
+    //2)
+    void inputAngle() {
+        std::cout << "Enter the degrees (integer): ";
+        std::cin >> degrees;
+        std::cout << "Enter the minut (integer): ";
+        std::cin >> minutes;
+        normalize();
+        assert(degrees >= 0 && degrees < 360);
+        assert(minutes >= 0 && minutes < 60);
+    }
+    std::string toString() const {
+        std::stringstream ss;
+        ss << degrees << "° " << minutes << "'";
+        return ss.str();
+    }
+    //3)
+    int getDegrees() const { return degrees; }
+    int getMinutes() const { return minutes; }
+
+    //4) 
+    double toRadians() const {
+        double totalDegrees = static_cast<double>(degrees) + static_cast<double>(minutes) / 60.0;
+        return (totalDegrees / 180.0) * M_PI;
     }
 };
